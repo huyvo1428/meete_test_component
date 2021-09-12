645d0435f01bf8f1c8051287c5edc0895cb92d5f
huy <huyvo.meete@gmail.com>
2021-09-13 03:08:23 +0700

    init

---
+++ go.mod
@@ -0,0 +1,15 @@
+module test
+
+go 1.17
+
+require bitbucket.org/meete/genesis-core v0.0.0-20210912144315-04bacb1b65a8
+
+require (
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/go-playground/validator/v10 v10.9.0 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/sys v0.0.0-20210806184541-e5e7981a1069 // indirect
+	golang.org/x/text v0.3.6 // indirect
+)
---
+++ main.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"bitbucket.org/meete/genesis-core/component/validation/goplayground"
+	"fmt"
+)
+
+func main() {
+	config := goplayground.Configuration{}
+	validation := goplayground.New(config)
+
+	//ok
+	fmt.Println("---------------------------------------")
+	fmt.Println("TEST1")
+
+	dataValidate := struct {
+		Phone string `json:"phone" validate:"required,phone"`
+	}{"0987654321"}
+
+	err := validation.Struct(dataValidate)
+
+	fmt.Println(err)
+
+	//Key: 'Phone' Error:Field validation for 'Phone' failed on the 'required' tag---ok
+	fmt.Println("\n\n---------------------------------------")
+	fmt.Println("TEST2")
+
+	dataValidate = struct {
+		Phone string `json:"phone" validate:"required,phone"`
+	}{"9876543210"}
+
+	err = validation.Struct(dataValidate)
+
+	fmt.Println(err)
+
+	//Key: 'Phone' Error:Field validation for 'Phone' failed on the 'required' tag---ok
+	fmt.Println("\n\n---------------------------------------")
+	fmt.Println("TEST3")
+
+	dataValidate = struct {
+		Phone string `json:"phone" validate:"required,phone"`
+	}{"098765432199"}
+
+	err = validation.Struct(dataValidate)
+
+	fmt.Println(err)
+
+	//Key: 'Phone' Error:Field validation for 'Phone' failed on the 'required' tag---ok
+	fmt.Println("\n\n---------------------------------------")
+	fmt.Println("TEST4")
+
+	dataValidate = struct {
+		Phone string `json:"phone" validate:"required,phone"`
+	}{"098765"}
+
+	err = validation.Struct(dataValidate)
+
+	fmt.Println(err)
+
+	//Key: 'Phone' Error:Field validation for 'Phone' failed on the 'required' tag---ok
+	fmt.Println("\n\n---------------------------------------")
+	fmt.Println("TEST5")
+
+	dataValidate = struct {
+		Phone string `json:"phone" validate:"required,phone"`
+	}{"98765"}
+
+	err = validation.Struct(dataValidate)
+
+	fmt.Println(err)
+
+	//Key: 'Phone' Error:Field validation for 'Phone' failed on the 'required' tag---ok
+	fmt.Println("\n\n---------------------------------------")
+	fmt.Println("TEST6")
+
+	dataValidate = struct {
+		Phone string `json:"phone" validate:"required,phone"`
+	}{"098765432A"}
+
+	err = validation.Struct(dataValidate)
+
+	fmt.Println(err)
+
+	//Key: 'Phone' Error:Field validation for 'Phone' failed on the 'required' tag---ok
+	fmt.Println("\n\n---------------------------------------")
+	fmt.Println("TEST7")
+
+	dataValidate = struct {
+		Phone string `json:"phone" validate:"required,phone"`
+	}{"09B76A4321"}
+
+	err = validation.Struct(dataValidate)
+
+	fmt.Println(err)
+
+	//Key: 'Phone' Error:Field validation for 'Phone' failed on the 'required' tag---ok
+	fmt.Println("\n\n---------------------------------------")
+	fmt.Println("TEST8")
+
+	dataValidate = struct {
+		Phone string `json:"phone" validate:"required,phone"`
+	}{"098765432-"}
+
+	err = validation.Struct(dataValidate)
+
+	fmt.Println(err)
+
+	//Key: 'Phone' Error:Field validation for 'Phone' failed on the 'required' tag---ok
+	fmt.Println("\n\n---------------------------------------")
+	fmt.Println("TEST9")
+
+	dataValidate = struct {
+		Phone string `json:"phone" validate:"required,phone"`
+	}{"0987-54321"}
+
+	err = validation.Struct(dataValidate)
+
+	fmt.Println(err)
+
+	//Key: 'Phone' Error:Field validation for 'Phone' failed on the 'required' tag---FAILD
+	fmt.Println("\n\n---------------------------------------")
+	fmt.Println("TEST9")
+
+	dataValidate1 := struct {
+		Phone string `json:"phone" validate:"phone"`
+	}{}
+
+	err = validation.Struct(dataValidate1)
+
+	fmt.Println(err)
+
+}
